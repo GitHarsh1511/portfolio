d306076770b81e9a0f0bfde8386457dcf7b5150c
Harsh Chauhan <Harsh Chauhan>
2025-12-13 21:21:54 +0530

    footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="border-t border-slate-800 py-6 text-center">
-      <p className="text-slate-400 text-sm">
-        © {new Date().getFullYear()} Harsh Chauhan. All rights reserved.
-      </p>
+    <footer className="border-t border-slate-800">
+      <div className="max-w-7xl mx-auto px-6 py-6 text-center">
+        <p className="text-slate-400 text-sm">
+          © {new Date().getFullYear()} Harsh Chauhan | All Rights Reserved.
+        </p>
+      </div>
     </footer>
   );
 }
